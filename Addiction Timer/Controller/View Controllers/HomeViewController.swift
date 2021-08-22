40d2450959914916b16fb4f9085fb6609bb51981
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-08-22 16:52:44 -0700

    stubbed out logic to present add habit view when no habits are loaded

--- Addiction Timer/Controller/View Controllers/HomeViewController.swift
+++ Addiction Timer/Controller/View Controllers/HomeViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 /// Lists Habits with timers
 /// if no habits are found, start onboarding
 class HomeViewController: UIViewController {
+    
+    var habits: [Habit] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // TODO: load habits
+        if habits.isEmpty {
+            // TODO: launch add habit wizard
+        }
     }
 
 
